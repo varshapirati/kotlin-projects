e696933f932e59191bbc46d5d6c067b75104d75f
varshapirati <pvtgoud@gmail.com>
2021-05-11 20:35:00 +0530

    My first commit

--- app/src/main/java/com/example/myapplication/main.kt
+++ app/src/main/java/com/example/myapplication/main.kt
@@ -2,5 +2,9 @@ package com.example.myapplication
 
 fun main()
 {
-    print("My world")
+    var myString:String="Android Kotlin"
+    var myFloat:Float=13.37F
+    val pi=3.14159265358979F
+    var myInt:Int=25
+    print("$myString \n $myFloat \n $pi \n $myInt")
 }
